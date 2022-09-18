9786217f9c85109b11f1e6fb1f4ab0ffb517888a
Ryan Slater <32707042+rslater-cs@users.noreply.github.com>
2022-09-18 21:49:45 +0100

    Created caching for faster loading of data

--- data/patch_loader.py
+++ data/patch_loader.py
@@ -10,11 +10,14 @@ import sys
 import random
 
 PATH_ROOT = Path(os.path.curdir) / "data" / "patches"
+PATCH_CACHE_SIZE = 100000
 
 class PatchSet(Dataset):
     def __init__(self, patch_size):
         self.PILtoTensor = transforms.ToTensor()
         self.path = PATH_ROOT / "{}_{}".format(patch_size[0], patch_size[1])
+        self.patch_size = patch_size
+        self.progress = 0
 
         self.content = []
 
@@ -27,10 +30,22 @@ class PatchSet(Dataset):
 
         random.shuffle(self.content)
 
+        self.cache = torch.zeros((PATCH_CACHE_SIZE, 3, self.patch_size[1], self.patch_size[0]))
+        self.load_patches(self.progress)
+
         print("Loading Complete")
+
+    def load_patches(self, start):
+        size = min(len(self.content)-start, PATCH_CACHE_SIZE)
+        for i in range(size):
+            self.cache[i] = self.PILtoTensor(Image.open(self.content[start+i]))
 
     def __len__(self):
         return len(self.content)
 
     def __getitem__(self, index):
-        return self.PILtoTensor(Image.open(self.content[index]))
+        if(index > self.progress+PATCH_CACHE_SIZE):
+            self.progress += PATCH_CACHE_SIZE
+            self.load_patches(self.progress)
+
+        return self.cache[index % PATCH_CACHE_SIZE]
