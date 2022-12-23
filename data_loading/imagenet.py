354f72f1db6eaa0d00bd9b07ecab99ebfcaa0907
Ryan Slater <ryan.slater157@gmail.com>
2022-12-23 11:01:59 +0000

    started AC and changed dataset path

--- data_loading/imagenet.py
+++ data_loading/imagenet.py
@@ -6,6 +6,9 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import Subset
 import numpy as np
 
+# ImageCompression -> AI -> Python -> Programming
+REL_PATH_TO_DATASETS = "..\\..\\..\\"
+
 class IN():
 
     def __init__(self, portion=2):
@@ -15,7 +18,7 @@ class IN():
             ToTensor(),
             # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
-        full_trainset = ImageFolder(root='E:\\Programming\Datasets\\ImageNet\\train', transform=transform)
+        full_trainset = ImageFolder(root=REL_PATH_TO_DATASETS+'Datasets\\ImageNet\\train', transform=transform)
         self.trainset = Subset(dataset=full_trainset, indices=np.random.choice(len(full_trainset), len(full_trainset)//portion, replace=False))
         # self.validset = ImageNet(root='E:\Programming\Datasets\train_blurred', train=False, download=True, transform=transform)
         self.shufflemode = True
