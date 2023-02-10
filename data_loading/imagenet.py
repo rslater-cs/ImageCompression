baf48473376e08f2b19c8be65b60bf95cb387990
Ryan Slater <ryan.slater157@gmail.com>
2023-02-10 11:56:04 +0000

    increase imagent size and lower batch

--- data_loading/imagenet.py
+++ data_loading/imagenet.py
@@ -8,7 +8,7 @@ class IN():
     def __init__(self, root):
         transform = transforms.Compose([
             transforms.Resize(256),
-            transforms.CenterCrop((112, 112)),
+            transforms.CenterCrop((224, 224)),
             ToTensor(),
             # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
