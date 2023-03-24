c79da88b4e1f9a1060fdc6b252924276e8caf743
Ryan Slater <ryan.slater157@gmail.com>
2023-03-24 19:56:20 +0000

    Update setup.sh

--- enviroment/setup.sh
+++ enviroment/setup.sh
@@ -2,4 +2,5 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda3 -s -u
 rm Miniconda3-latest-Linux-x86_64.sh
 conda=miniconda3/bin/conda
+$conda remove --name myenv --all
 $conda env create -f '/user/HS225/rs01241/Documents/disso/ImageCompression/enviroment/ubuntu_env.yml'
