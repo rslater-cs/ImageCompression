107c20b819afadebbcbad746973efda3c47a62d1
Ryan Slater <ryan.slater157@gmail.com>
2023-03-24 20:00:08 +0000

    created system to update a environment

--- enviroment/setup.sh
+++ enviroment/setup.sh
@@ -2,5 +2,4 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda3 -s -u
 rm Miniconda3-latest-Linux-x86_64.sh
 conda=miniconda3/bin/conda
-$conda remove --name ml_compression --all
 $conda env create -f '/user/HS225/rs01241/Documents/disso/ImageCompression/enviroment/ubuntu_env.yml'
---
+++ enviroment/update_env.sh
@@ -0,0 +1,3 @@
+conda=miniconda3/bin/conda
+$conda remove --name ml_compression --all
+$conda env create -f '/user/HS225/rs01241/Documents/disso/ImageCompression/enviroment/ubuntu_env.yml'
