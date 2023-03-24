80ba1855f9840475084b96a4dd85e7ff5de454a6
Ryan Slater <ryan.slater157@gmail.com>
2023-03-24 20:20:27 +0000

    Update update_env.sh

--- enviroment/update_env.sh
+++ enviroment/update_env.sh
@@ -1,3 +1,6 @@
+rm -r miniconda3/bin/conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda3 -s -u
+rm Miniconda3-latest-Linux-x86_64.sh
 conda=miniconda3/bin/conda
-$conda remove --name ml_compression --all
 $conda env create -f '/user/HS225/rs01241/Documents/disso/ImageCompression/enviroment/ubuntu_env.yml'
